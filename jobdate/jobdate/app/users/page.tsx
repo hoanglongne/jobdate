72cc9e5334db103a98fbafc0b51d697a8a0b6aef
hoanglongne <73406617+hoanglongne@users.noreply.github.com>
2024-05-04 15:50:16 +0700

    First commit

---
+++ jobdate/jobdate/app/users/page.tsx
@@ -0,0 +1,10 @@
+import { createClient } from '@/utils/supabase/server';
+
+export default async function Users() {
+    const supabase = createClient();
+    console.log(supabase)
+    const { data: users } = await supabase.from("users").select(`*`);
+
+    return <pre>{JSON.stringify(users, null, 2)}</pre>
+
+}
